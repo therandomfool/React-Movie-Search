b848c2fa94ee73cf5c230b2dfa960f5a84adc130
therandomfool <mddenver@protonmail.com>
2021-04-04 14:46:17 -0700

    Finished README

---
+++ README.md
@@ -0,0 +1,13 @@
+# React Movie Search
+
+- using themoviedb API
+- Featured API for most popular (on load)
+- Search API for search movies info (from search bar)
+- Image API for images for all movies
+- Average rating color scaled on:
+- 8> = green
+- 6> = orange
+- 6< = red
+
+
+![React Movie Search](assets/movie.gif)
---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, {
+  useEffect,
+  useState
+} from 'react'
+import Movie from './components/Movie';
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6c5b2b0575017f681663780da0ba6a07&page-1"
+
+
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=6c5b2b0575017f681663780da0ba6a07&query="
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+  
+  const getMovies = (API) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
+    
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if(searchTerm) {
+      getMovies(SEARCH_API + searchTerm)
+
+      setSearchTerm('');
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value)
+  };
+
+  return (
+    <>
+    <header>
+      <form onSubmit={handleOnSubmit}>
+        <input className='search' 
+        type='search' 
+        placeholder='Search ...'
+        value = {searchTerm}
+        onChange={handleOnChange}
+        />
+        
+      </form>
+    </header>
+        <div className = 'movie-container'>
+          {movies.length > 0 && 
+            movies.map((movie) => <  Movie key = {
+            movie.id} {...movie}/>)}
+        </div>
+      </>
+    );
+}
+
+      export default App;
